06bb1e621782f4d70f85ae94c40398ff3264b212
kenjiskywalker <git@kenjiskywalker.org>
2014-06-08 01:42:31 +0900

    Use dameleon's code. leave old code, and leave all comment.

--- content.js
+++ content.js
@@ -1,31 +1,3 @@
-// 1. hoge より良い名前つけましょう。例えば、jQuery のオブジェクトなら $age = $('.age') とかにするのが良いです。あと .age だけだとセレクタとして弱く、変なところに色付くかもしれないです。 '.files .age' とか？
-// 2. ファイル内でいきなり変数宣言してしまうと、グローバル変数になってしまうので全体を無名関数で囲み、クロージャを生成して、即時実行する。(下記コード参照のこと)
-var hoge = $('.age');
-
-// 3. $('.age').each(function(_, value) {}) って書けます
-$.each(hoge, function(_, value){
-    console.log(value);
-    // 4. ここ、jQuery に食わせる意味ないと思います。それよりは var $value = $(value) として、$value.text() で良いのでは。あと一応 trim して空白除去しましょう。
-    var hoge = $(value.outerText);
-    // 5. 変数名気になる
-    var a = hoge.selector;
-    if(a.indexOf("year") !== -1){
-        // 6. color に渡す value は、定数にしておいた方がよさそう。なんなら、設定値としてオブジェクトで持ってた方がいいのでは。(下記コード参照のこと)
-        $(value).css({'color': 'red'});
-    }else if(a.indexOf("months ago") !== -1){
-        // 7. これ正規表現おかしくないですか
-        if(/([5-9|1(0|1)])/.test(a)) {
-            $(value).css({'color': 'orange'});
-        }
-    }else{
-        // 8. 設定値が一つの場合は .css('color', '#888') で良いかと
-        $(value).css({'color': '#888'});
-    }
-});
-
-
-/** オマケ '[]'カッコ内は上記指摘番号
-
 // window (グローバル変数) を global として受け取る [2]
 (function(global) {
 
@@ -62,4 +34,28 @@ $('.files .age').each(function(_, value) {
 });
 
 })(window);
-*/
+
+// // 1. hoge より良い名前つけましょう。例えば、jQuery のオブジェクトなら $age = $('.age') とかにするのが良いです。あと .age だけだとセレクタとして弱く、変なところに色付くかもしれないです。 '.files .age' とか？
+// // 2. ファイル内でいきなり変数宣言してしまうと、グローバル変数になってしまうので全体を無名関数で囲み、クロージャを生成して、即時実行する。(下記コード参照のこと)
+// var hoge = $('.age');
+//
+// // 3. $('.age').each(function(_, value) {}) って書けます
+// $.each(hoge, function(_, value){
+//     console.log(value);
+//     // 4. ここ、jQuery に食わせる意味ないと思います。それよりは var $value = $(value) として、$value.text() で良いのでは。あと一応 trim して空白除去しましょう。
+//     var hoge = $(value.outerText);
+//     // 5. 変数名気になる
+//     var a = hoge.selector;
+//     if(a.indexOf("year") !== -1){
+//         // 6. color に渡す value は、定数にしておいた方がよさそう。なんなら、設定値としてオブジェクトで持ってた方がいいのでは。(下記コード参照のこと)
+//         $(value).css({'color': 'red'});
+//     }else if(a.indexOf("months ago") !== -1){
+//         // 7. これ正規表現おかしくないですか
+//         if(/([5-9|1(0|1)])/.test(a)) {
+//             $(value).css({'color': 'orange'});
+//         }
+//     }else{
+//         // 8. 設定値が一つの場合は .css('color', '#888') で良いかと
+//         $(value).css({'color': '#888'});
+//     }
+// });
